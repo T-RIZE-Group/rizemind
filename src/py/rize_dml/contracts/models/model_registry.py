c9c31c8d7622e7ae9b9e231c8e91ae607cf1bf69
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-02-14 14:15:50 -0500

    Feature: Add get_eip712_domain to ModelRegistery

--- src/py/rize_dml/contracts/models/model_registry.py
+++ src/py/rize_dml/contracts/models/model_registry.py
@@ -1,16 +1,38 @@
+from typing import List, NamedTuple
 from eth_typing import Address
 from web3.contract import Contract
 
-class ModelRegistry():
 
-  model: Contract
+class EIP712Domain(NamedTuple):
+    fields: bytes
+    name: str
+    version: str
+    chainId: int
+    verifyingContract: str
+    salt: bytes
+    extensions: List[int]
 
-  def __init__(self, model: Contract):
-    self.model = model
 
-  def can_train(self, trainer: Address, round_id: int) -> bool:
-    return self.model.functions.canTrain(trainer, round_id).call()
-  
-  def current_round(self) -> int:
-    return self.model.functions.currentRound().call()
+class ModelRegistry:
+    model: Contract
 
+    def __init__(self, model: Contract):
+        self.model = model
+
+    def can_train(self, trainer: Address, round_id: int) -> bool:
+        return self.model.functions.canTrain(trainer, round_id).call()
+
+    def current_round(self) -> int:
+        return self.model.functions.currentRound().call()
+
+    def get_eip712_domain(self) -> EIP712Domain:
+        resp = self.model.functions.eip712Domain().call()
+        return EIP712Domain(
+            fields=resp.fields,
+            name=resp.name,
+            version=resp.version,
+            chainId=resp.chainId,
+            verifyingContract=resp.verifyingContract,
+            salt=resp.salt,
+            extensions=resp.extensions,
+        )
