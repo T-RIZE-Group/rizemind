c7410eb76921ec2512273b26e885e1492201dc02
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-02-14 08:29:55 -0500

    Fix: Prettier

--- smart_contracts/contracts/access_control/FLAccessControl.sol
+++ smart_contracts/contracts/access_control/FLAccessControl.sol
@@ -2,52 +2,54 @@
 pragma solidity ^0.8.10;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import { Initializable } from "@ozupgradeable/contracts/proxy/utils/Initializable.sol";
+import {Initializable} from "@ozupgradeable/contracts/proxy/utils/Initializable.sol";
 
 contract FLAccessControl is AccessControl, Initializable {
+    bytes32 constant AGGREGATOR_ROLE = keccak256("AGGREGATOR");
+    bytes32 constant TRAINER_ROLE = keccak256("TRAINER");
 
-  bytes32 constant AGGREGATOR_ROLE = keccak256("AGGREGATOR");
-  bytes32 constant TRAINER_ROLE = keccak256("TRAINER");
-
-  modifier onlyTrainer(address account) {
-    _checkTrainer(account);
-    _;
-  }
-
-  modifier onlyAggregator(address account) {
-    _checkAggregator(account);
-    _;
-  }
-
-  function __FLAccessControl_init(address aggregator, address[] memory initialTrainers) internal onlyInitializing {
-    _grantRole(AGGREGATOR_ROLE, aggregator);
-    for(uint8 i = 0; i < initialTrainers.length; i++) {
-      _grantRole(TRAINER_ROLE, initialTrainers[i]);
+    modifier onlyTrainer(address account) {
+        _checkTrainer(account);
+        _;
     }
-  }
 
-  function isTrainer(address trainer) public view returns(bool) {
-    return hasRole(TRAINER_ROLE, trainer);
-  }
+    modifier onlyAggregator(address account) {
+        _checkAggregator(account);
+        _;
+    }
 
-  function _checkTrainer(address trainer) internal view virtual {
-    _checkRole(TRAINER_ROLE, trainer);
-  }
+    function __FLAccessControl_init(
+        address aggregator,
+        address[] memory initialTrainers
+    ) internal onlyInitializing {
+        _grantRole(AGGREGATOR_ROLE, aggregator);
+        for (uint8 i = 0; i < initialTrainers.length; i++) {
+            _grantRole(TRAINER_ROLE, initialTrainers[i]);
+        }
+    }
 
-  function isAggregator(address aggregator) public view returns(bool) {
-    return hasRole(AGGREGATOR_ROLE, aggregator);
-  }
+    function isTrainer(address trainer) public view returns (bool) {
+        return hasRole(TRAINER_ROLE, trainer);
+    }
 
-  function _checkAggregator(address aggregator) internal view virtual {
-    _checkRole(AGGREGATOR_ROLE, aggregator);
-  }
+    function _checkTrainer(address trainer) internal view virtual {
+        _checkRole(TRAINER_ROLE, trainer);
+    }
 
+    function isAggregator(address aggregator) public view returns (bool) {
+        return hasRole(AGGREGATOR_ROLE, aggregator);
+    }
+
+    function _checkAggregator(address aggregator) internal view virtual {
+        _checkRole(AGGREGATOR_ROLE, aggregator);
+    }
 }
 
 contract InitializableFLAccessControl is FLAccessControl {
-
-  function initialize(address aggregator, address[] memory trainers) public initializer {
-    __FLAccessControl_init(aggregator, trainers);
-  }
-
+    function initialize(
+        address aggregator,
+        address[] memory trainers
+    ) public initializer {
+        __FLAccessControl_init(aggregator, trainers);
+    }
 }
--- smart_contracts/contracts/models/IModelRegistry.sol
+++ smart_contracts/contracts/models/IModelRegistry.sol
@@ -1,13 +1,12 @@
-
 struct RoundSummary {
-  uint256 roundId;
-  uint64 nTrainers;
-  uint64 modelScore;
-  uint128 totalContributions;
+    uint256 roundId;
+    uint64 nTrainers;
+    uint64 modelScore;
+    uint128 totalContributions;
 }
 
 interface IModelRegistry {
     function canTrain(address trainer, uint256 roundId) external returns (bool);
-    function curentRound() external view returns(uint256);
+    function curentRound() external view returns (uint256);
     function nextRound(RoundSummary calldata summary) external;
 }
