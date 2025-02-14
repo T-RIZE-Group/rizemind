8b5c7cb81854bea2af5d28a9fa895cd043f89e02
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-02-14 13:49:22 -0500

    Refactor: Use ModelRegisteryV1 to get eip712 parameters instead of hardcoding

--- src/py/rize_dml/authentication/eth_account_client.py
+++ src/py/rize_dml/authentication/eth_account_client.py
@@ -1,58 +1,65 @@
-from typing import Tuple
+from typing import Dict
 from flwr.client import Client
 from flwr.common import FitRes, FitIns, EvaluateIns
+from rize_dml.authentication.signature import (
+    sign_parameters_model,
+)
+from rize_dml.contracts.models.model_registry_v1 import ModelRegistryV1
 from eth_account import Account
-from eth_account.datastructures import SignedMessage
-from rize_dml.authentication.signature import sign_parameters_model
 
 
 class SigningClient:
+    client: Client
+    model: ModelRegistryV1
+    account: Account
 
-  client: Client
-  account: Account
-  chainid: int
-  contract: str
-  name: str
+    def __init__(self, client: Client, account: Account):
+        contract_address = str(FitIns.config["address"])
+        self.model = ModelRegistryV1.from_address(contract_address)
+        self.client = client
+        self.account = account
 
-  def __init__(
-    self, 
-    client: Client,
-    account: Account,
-    chainid: int,
-    contract: str,
-    name: str
-  ):
-    self.client = client
-    self.account = account
-    self.chainid = chainid
-    self.contract = contract
-    self.name = name
+    def __getattr__(self, name):
+        return getattr(self.client, name)
 
-  def __getattr__(self, name):
-    return getattr(self.client, name)
+    def fit(self, ins: FitIns):
+        # Call the original fit method on the proxied Client
+        results: FitRes = self.client.fit(ins)
 
-  def fit(self, ins: FitIns):
-    # Call the original fit method on the proxied Client
-    results: FitRes = self.client.fit(ins)
-    signature = self._sign(results)
-    results.metrics = results.metrics | signature
-    return results
-  
-  def _sign(self, res: FitRes) -> SignedMessage:
-    
-    signature = sign_parameters_model(
-      self.account,
-      res.parameters,
-      self.chainid,
-      self.contract,
-      self.name,
-      0    
-    )
-    return {
-      "r": signature.r.to_bytes(32, byteorder='big'),
-      "s": signature.s.to_bytes(32, byteorder='big'),
-      "v": signature.v.to_bytes(1, byteorder='big')
-    }
-  
-  def evaluate(self, ins: EvaluateIns):
-    return self.client.evaluate(ins)
+        round = ins.config["current_round"]
+        round_in_int = ensure_int(round)
+        signature = self._sign(results, round_in_int)
+
+        results.metrics = results.metrics | signature
+        return results
+
+    def _sign(self, res: FitRes, round: int) -> Dict[str, bytes]:
+        eip712_domain = self.model.get_eip712_domain()
+        # Output Signer
+        signature = sign_parameters_model(
+            account=self.account,
+            parameters=res.parameters,
+            name=eip712_domain.name,
+            chainid=eip712_domain.chainId,
+            contract=eip712_domain.verifyingContract,
+            version=eip712_domain.version,
+            round=round,
+        )
+        return {
+            "r": signature.r.to_bytes(32, byteorder="big"),
+            "s": signature.s.to_bytes(32, byteorder="big"),
+            "v": signature.v.to_bytes(1, byteorder="big"),
+        }
+
+    def evaluate(self, ins: EvaluateIns):
+        return self.client.evaluate(ins)
+
+
+def ensure_int(value) -> int:
+    if value is None:
+        raise ValueError("Value must not be None")
+    if isinstance(value, int):
+        return value
+    if isinstance(value, (bool, bytes, float, str)):
+        return int(value)
+    raise ValueError(f"Cannot convert value of type {type(value)} to int")
