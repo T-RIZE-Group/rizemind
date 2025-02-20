042944ee67ffde3a78d6ad1e879ab47132b8a1dc
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-02-20 09:20:31 -0500

    Refactor: Change compensation strategy to use a regular strategy and be wrapped by eth account strategy

--- src/py/rize_dml/contracts/compensation/compensation_strategy.py
+++ src/py/rize_dml/contracts/compensation/compensation_strategy.py
@@ -1,17 +1,19 @@
 from abc import abstractmethod
-from typing import Any
 from eth_typing import Address
-from rize_dml.authentication.eth_account_strategy import EthAccountStrategy
+
+# from rize_dml.authentication.eth_account_strategy import EthAccountStrategy
 from flwr.server.strategy import Strategy
+from rize_dml.contracts.models.model_registry_v1 import ModelRegistryV1
 
 
 class CompensationStrategy(Strategy):
-    strategy: EthAccountStrategy
+    strategy: Strategy
+    model: ModelRegistryV1
 
-    def __init__(self, strategy: EthAccountStrategy) -> None:
-        "Takes a strategy"
+    def __init__(self, strategy: Strategy, model: ModelRegistryV1) -> None:
         self.strategy = strategy
+        self.model = model
 
     @abstractmethod
-    def calculate(self, client_ids: list[Address]) -> tuple[list[Address], list[Any]]:
+    def calculate(self, client_ids: list[Address]) -> tuple[list[Address], list[int]]:
         "Calculates and returns the compensation for each client in the list"
--- src/py/rize_dml/contracts/compensation/simple_compensation_startegy.py
+++ src/py/rize_dml/contracts/compensation/simple_compensation_startegy.py
@@ -1,32 +1,29 @@
+from typing import cast
 from eth_typing import Address
 from flwr.common import EvaluateIns, EvaluateRes, FitIns, Parameters
 from flwr.server.client_manager import ClientManager
-from rize_dml.authentication.eth_account_strategy import EthAccountStrategy
+from flwr.server.strategy import Strategy
+
 from rize_dml.contracts.compensation.compensation_strategy import (
     CompensationStrategy,
 )
-from flwr.common.typing import FitRes
 from flwr.server.client_proxy import ClientProxy
+from rize_dml.contracts.models.model_registry_v1 import ModelRegistryV1
 
 
 class SimpleCompensationStrategy(CompensationStrategy):
-    def __init__(self, strategy: EthAccountStrategy):
-        CompensationStrategy.__init__(self, strategy)
+    def __init__(self, strategy: Strategy, model: ModelRegistryV1):
+        CompensationStrategy.__init__(self, strategy, model)
 
     def calculate(self, client_ids: list[Address]):
         return client_ids, [1 for _ in client_ids]
 
     def aggregate_fit(self, server_round, results, failures):
-        whitelisted: list[tuple[ClientProxy, FitRes]] = []
-        whitelisted_address: list[Address] = []
-        for client, res in results:
-            signer = self.strategy._recover_signer(res, server_round)
-            if self.strategy.model.can_train(signer, server_round):
-                whitelisted.append((client, res))
-                whitelisted_address.append(signer)
-        trainers, contributions = self.calculate(whitelisted_address)
-        self.strategy.model.distribute(trainers, contributions)
-        return self.strategy.strat.aggregate_fit(server_round, whitelisted, failures)
+        trainers, contributions = self.calculate(
+            [cast(Address, res.metrics["trainer_address"]) for _, res in results]
+        )
+        self.model.distribute(trainers, contributions)
+        return self.strategy.aggregate_fit(server_round, results, failures)
 
     def initialize_parameters(self, client_manager: ClientManager) -> Parameters | None:
         return self.strategy.initialize_parameters(client_manager)
