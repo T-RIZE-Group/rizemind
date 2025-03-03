2a0faede6dc273f6028a7b4ccb9f75d8d34ac301
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-03-03 14:01:46 -0500

    Tidyup: Add comment to shapely_value_strategy compute_contributions

--- src/py/rizemind/contracts/compensation/shapely_value_strategy.py
+++ src/py/rizemind/contracts/compensation/shapely_value_strategy.py
@@ -41,8 +41,11 @@ class ShapelyValueStrategy(CompensationStrategy):
         self, coalition_and_scores: list[CoalitionScore]
     ) -> list[PlayerScore]:
         # Create a bijective mapping between addresses and a bit_based representation
+        # First the coalition_and_scores is sorted based on the length of list of addresses
+        # Then given that the largest list has all addresses, it will assign it to
+        # list_of_addresses
         coalition_and_scores.sort(key=lambda v: len(v[0]))
-        list_of_addresses = coalition_and_scores[-1][0]
+        list_of_addresses, _ = coalition_and_scores[-1]
         address_map: bidict = bidict()
         bit = 0b1
         for address in list_of_addresses:
