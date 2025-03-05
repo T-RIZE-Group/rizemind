d5b4e4333526d628204b62c71577348e5270adc2
Mikaeil-rize <mikaeil.mayeli@t-rize.io>
2025-03-05 13:47:16 -0500

    Feature: Create a decentral example of shapely value

---
+++ examples/decentralized_shapely_value/decentralized_shapley_value/server_app.py
@@ -0,0 +1,69 @@
+"""signedupdates: A Flower / TensorFlow app."""
+
+from flwr.common import Context, Metrics, ndarrays_to_parameters
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedAvg
+from rizemind.authentication.config import AccountConfig
+from rizemind.contracts.compensation.decentral_shapely_value_strategy import (
+    DecentralShapelyValueStrategy,
+)
+from rizemind.web3.config import Web3Config
+from rizemind.configuration.toml_config import TomlConfig
+from rizemind.contracts.models.model_factory_v1 import (
+    ModelFactoryV1Config,
+    ModelFactoryV1,
+)
+from .task import load_model
+from rizemind.authentication.eth_account_strategy import EthAccountStrategy
+
+
+# Define metric aggregation function
+def weighted_average(metrics: list[tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * float(m["accuracy"]) for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+def server_fn(context: Context):
+    """Construct components that set the ServerApp behaviour."""
+
+    # Let's define the global model and pass it to the strategy
+    # Note this is optional.
+    parameters = ndarrays_to_parameters(load_model().get_weights())
+    print(context.run_config)
+    print(context.node_config)
+    # Define the strategy
+    strategy = FedAvg(
+        fraction_fit=float(context.run_config["fraction-fit"]),
+        fraction_evaluate=1.0,
+        min_available_clients=2,
+        initial_parameters=parameters,
+        evaluate_metrics_aggregation_fn=weighted_average,
+    )
+    # Read from config
+    num_rounds = int(context.run_config["num-server-rounds"])
+    config = TomlConfig("./pyproject.toml")
+    auth_config = AccountConfig(**config.get("tool.eth.account"))
+    web3_config = Web3Config(**config.get("tool.web3"))
+    w3 = web3_config.get_web3()
+    account = auth_config.get_account(0)
+    members = []
+    for i in range(1, 11):
+        trainer = auth_config.get_account(i)
+        members.append(trainer.address)
+
+    model_v1_config = ModelFactoryV1Config(**config.get("tool.web3.model_v1"))
+    contract = ModelFactoryV1(model_v1_config).deploy(account, members, w3)
+    config = ServerConfig(num_rounds=int(num_rounds))
+    authStrategy = EthAccountStrategy(
+        DecentralShapelyValueStrategy(strategy, contract, parameters, 1.0, 3, 3),
+        contract,
+    )
+    return ServerAppComponents(strategy=authStrategy, config=config)
+
+
+# Create ServerApp
+app = ServerApp(server_fn=server_fn)
