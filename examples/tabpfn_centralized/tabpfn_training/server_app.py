e6216c097dbf65ab8a48ad8847bfe49c70b02175
MikaeelMF <mikaeelmayeli@gmail.com>
2025-07-03 15:43:05 -0400

    Create tabpfn server app

---
+++ examples/tabpfn_centralized/tabpfn_training/server_app.py
@@ -0,0 +1,33 @@
+from flwr.common import Context, Metrics, ndarrays_to_parameters
+from flwr.server import ServerAppComponents, ServerConfig
+from flwr.server.strategy import FedAvg
+from tabpfn.regressor import TabPFNRegressor
+from tabpfn_centralized.tabpfn_training.strategy import SimpleTabPFNRegressorStrategy
+from tabpfn_centralized.tabpfn_training.task import get_weights
+
+
+def weighted_average(metrics: list[tuple[int, Metrics]]) -> Metrics:
+    r2_scores = [num_examples * float(m["r2_score"]) for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    return {"weighted_average_r2_score": sum(r2_scores) / sum(examples)}
+
+
+def server_fn(context: Context):
+    num_rounds = int(context.run_config["num-server-rounds"])
+
+    model_path = str(context.run_config["initial-model-path"])
+    ndarrays = get_weights(TabPFNRegressor(model_path=model_path).model_)
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    config = ServerConfig(num_rounds)
+    strategy = FedAvg(
+        fraction_fit=float(context.run_config["fraction-fit"]),
+        fraction_evaluate=float(context.run_config["fraction-evaluate"]),
+        min_available_clients=int(context.run_config["min-available-clients"]),
+        evaluate_metrics_aggregation_fn=weighted_average,
+        initial_parameters=parameters,
+    )
+    tabpfn_strategy = SimpleTabPFNRegressorStrategy(
+        strategy=strategy, base_model_path=model_path
+    )
+    return ServerAppComponents(strategy=tabpfn_strategy, config=config)
---
+++ examples/tabpfn_centralized/tabpfn_training/strategy.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+from typing import cast
+from flwr.common import (
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    Parameters,
+    Scalar,
+    parameters_to_ndarrays,
+)
+from flwr.server import ClientManager
+from flwr.server.client_proxy import ClientProxy
+from flwr.server.strategy import Strategy
+from tabpfn.model.loading import load_model
+
+from tabpfn_centralized.tabpfn_training.client_app import (
+    load_weights_into_model,
+)
+import torch
+
+
+class SimpleTabPFNRegressorStrategy(Strategy):
+    strategy: Strategy
+    base_model_path: str
+
+    def __init__(self, *, strategy: Strategy, base_model_path: str) -> None:
+        self.strategy = strategy
+        self.base_model_path
+
+    def initialize_parameters(self, client_manager: ClientManager) -> Parameters | None:
+        return self.strategy.initialize_parameters(client_manager)
+
+    def configure_fit(
+        self, server_round: int, parameters: Parameters, client_manager: ClientManager
+    ) -> list[tuple[ClientProxy, FitIns]]:
+        return self.strategy.configure_fit(server_round, parameters, client_manager)
+
+    def aggregate_fit(
+        self, server_round, results, failures
+    ) -> tuple[Parameters | None, dict[str, Scalar]]:
+        parameters, res = self.strategy.aggregate_fit(server_round, results, failures)
+        model, _, checkpoint_config = load_model(
+            path=Path(self.base_model_path), model_seed=42
+        )
+        np_parameters = parameters_to_ndarrays(cast(Parameters, parameters))
+        model = load_weights_into_model(
+            model=model, parameters=np_parameters, config={}
+        )
+        torch.save(
+            dict(
+                model.state_dict(),
+                config=checkpoint_config,
+            ),
+            str(self.base_model_path),
+        )
+        return parameters, res
+
+    def configure_evaluate(
+        self, server_round: int, parameters: Parameters, client_manager: ClientManager
+    ) -> list[tuple[ClientProxy, EvaluateIns]]:
+        return self.strategy.configure_evaluate(
+            server_round, parameters, client_manager
+        )
+
+    def aggregate_evaluate(
+        self,
+        server_round: int,
+        results: list[tuple[ClientProxy, EvaluateRes]],
+        failures: list[tuple[ClientProxy, EvaluateRes] | BaseException],
+    ) -> tuple[float | None, dict[str, Scalar]]:
+        return self.strategy.aggregate_evaluate(server_round, results, failures)
+
+    def evaluate(
+        self, server_round: int, parameters: Parameters
+    ) -> tuple[float, dict[str, bool | bytes | float | int | str]] | None:
+        return self.strategy.evaluate(server_round, parameters)
