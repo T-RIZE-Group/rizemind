2d1ca67e88d9ef8cad0dc13e23dfbd1683e76fc6
mick00 <20918417+Mick00@users.noreply.github.com>
2025-07-07 12:51:23 -0400

    move configuration unit tests, add eth address pydantic model

---
+++ src/py/rizemind/configuration/validators/eth_address.py
@@ -0,0 +1,21 @@
+from eth_typing import ChecksumAddress
+from web3 import Web3
+from pydantic.functional_validators import AfterValidator
+from typing import Annotated
+
+
+def _validate_eth_address(addr: str) -> ChecksumAddress:
+    """
+    Ensure `addr` is a valid Ethereum address and
+    always return it in EIP‑55 checksum form.
+    """
+    if not isinstance(addr, str):
+        raise TypeError("Address must be a string")
+
+    if not Web3.is_address(addr):
+        raise ValueError("Invalid Ethereum address")
+
+    return Web3.to_checksum_address(addr)
+
+
+EthereumAddress = Annotated[str, AfterValidator(_validate_eth_address)]
