5f8f1d95d1849bf0c398e60088ddd3c3bc73c4cd
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-11 14:24:02 -0400

    adapt MetricsStorageStrategy to use the new BaseMetricsStorage

--- src/py/rizemind/logging/metrics_storage_strategy.py
+++ src/py/rizemind/logging/metrics_storage_strategy.py
@@ -1,14 +1,35 @@
+from enum import Enum
+from logging import warning
+
 from flwr.common import EvaluateIns, EvaluateRes, FitIns, FitRes, Parameters, Scalar
 from flwr.server import ClientManager
 from flwr.server.strategy import Strategy
 from rizemind.authentication.eth_account_strategy import ClientProxy
-from rizemind.logging.metrics_storage import MetricsStorage
+from rizemind.logging.base_metrics_storage import BaseMetricsStorage
+
+
+class MetricPhases(Enum):
+    AGGREGATE_FIT = 1
+    AGGREGATE_EVALUATE = 2
+    EVALUATE = 3
 
 
 class MetricsStorageStrategy(Strategy):
-    def __init__(self, strategy: Strategy, metrics_storage: MetricsStorage):
+    def __init__(
+        self,
+        strategy: Strategy,
+        metrics_storage: BaseMetricsStorage,
+        enabled_metric_phases: list[MetricPhases] = [
+            MetricPhases.AGGREGATE_FIT,
+            MetricPhases.AGGREGATE_EVALUATE,
+            MetricPhases.EVALUATE,
+        ],
+        save_best_model: bool = True,
+    ):
         self.strategy = strategy
         self.metrics_storage = metrics_storage
+        self.enabled_metric_phases = enabled_metric_phases
+        self.save_best_model = save_best_model
 
     def initialize_parameters(self, client_manager: ClientManager) -> Parameters | None:
         return self.strategy.initialize_parameters(client_manager)
@@ -16,11 +37,7 @@ class MetricsStorageStrategy(Strategy):
     def configure_fit(
         self, server_round: int, parameters: Parameters, client_manager: ClientManager
     ) -> list[tuple[ClientProxy, FitIns]]:
-        res = self.strategy.configure_fit(server_round, parameters, client_manager)
-        metrics_list = [metrics.config for _, metrics in res]
-        for metrics in metrics_list:
-            self.metrics_storage.write_metrics(server_round, metrics)
-        return res
+        return self.strategy.configure_fit(server_round, parameters, client_manager)
 
     def aggregate_fit(
         self,
@@ -31,17 +48,21 @@ class MetricsStorageStrategy(Strategy):
         parameters, metrics = self.strategy.aggregate_fit(
             server_round, results, failures
         )
-        self.metrics_storage.write_metrics(server_round, metrics)
+        if self.save_best_model:
+            if parameters is None:
+                warning("No model parameter provided, best model will not be saved.")
+            else:
+                self.metrics_storage.update_current_round_model(parameters)
+        if MetricPhases.AGGREGATE_FIT in self.enabled_metric_phases:
+            self.metrics_storage.write_metrics(server_round, metrics)
         return (parameters, metrics)
 
     def configure_evaluate(
         self, server_round: int, parameters: Parameters, client_manager: ClientManager
     ) -> list[tuple[ClientProxy, EvaluateIns]]:
-        res = self.strategy.configure_evaluate(server_round, parameters, client_manager)
-        metrics_list = [metrics.config for _, metrics in res]
-        for metrics in metrics_list:
-            self.metrics_storage.write_metrics(server_round, metrics)
-        return res
+        return self.strategy.configure_evaluate(
+            server_round, parameters, client_manager
+        )
 
     def aggregate_evaluate(
         self,
@@ -52,19 +73,23 @@ class MetricsStorageStrategy(Strategy):
         evaluation, metrics = self.strategy.aggregate_evaluate(
             server_round, results, failures
         )
-        if evaluation is not None:
-            self.metrics_storage.write_metrics(
-                server_round, {"loss_aggregated": evaluation}
-            )
-        self.metrics_storage.write_metrics(server_round, metrics)
+        if MetricPhases.AGGREGATE_EVALUATE in self.enabled_metric_phases:
+            if evaluation is not None:
+                self.metrics_storage.write_metrics(
+                    server_round, {"loss_aggregated": evaluation}
+                )
+            self.metrics_storage.write_metrics(server_round, metrics)
         return (evaluation, metrics)
 
     def evaluate(
         self, server_round: int, parameters: Parameters
     ) -> tuple[float, dict[str, Scalar]] | None:
         evaluation_result = self.strategy.evaluate(server_round, parameters)
-        if evaluation_result is None:
-            return None
-        self.metrics_storage.write_metrics(server_round, {"loss": evaluation_result[0]})
-        self.metrics_storage.write_metrics(server_round, evaluation_result[1])
+        if MetricPhases.EVALUATE in self.enabled_metric_phases:
+            if evaluation_result is None:
+                return None
+            self.metrics_storage.write_metrics(
+                server_round, {"loss": evaluation_result[0]}
+            )
+            self.metrics_storage.write_metrics(server_round, evaluation_result[1])
         return evaluation_result
