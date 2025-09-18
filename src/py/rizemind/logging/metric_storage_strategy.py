08a22ebe306ae845abaee83573c573d1a8c08b1b
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-18 15:41:51 -0400

    add docstring for metric_storage_strategy

--- src/py/rizemind/logging/metric_storage_strategy.py
+++ src/py/rizemind/logging/metric_storage_strategy.py
@@ -18,12 +18,16 @@ from rizemind.logging.base_metric_storage import BaseMetricStorage
 
 
 class MetricPhases(Enum):
+    """MetricPhases based on phases that the ServerApp receives metrics from the clients"""
+
     AGGREGATE_FIT = 1
     AGGREGATE_EVALUATE = 2
     EVALUATE = 3
 
 
 class MetricStorageStrategy(Strategy):
+    """The `MetricStorageStrategy` capable of logging metrics at `MetricPhases` given a metric storage."""
+
     def __init__(
         self,
         strategy: Strategy,
@@ -35,18 +39,26 @@ class MetricStorageStrategy(Strategy):
         ],
         save_best_model: bool = True,
     ):
-        self.strategy = strategy
-        self.metrics_storage = metrics_storage
-        self.enabled_metric_phases = enabled_metric_phases
-        self.save_best_model = save_best_model
+        """Initializes a MetricStorageStrategy.
+
+        Args:
+            strategy: The base Flower strategy to wrap.
+            metrics_storage: The storage object for writing metrics.
+            enabled_metric_phases: A list of phases during which metrics will be logged. Defaults to all phases.
+            save_best_model: A boolean indicating whether to save the best model parameters. Defaults to True.
+        """
+        self._strategy = strategy
+        self._metrics_storage = metrics_storage
+        self._enabled_metric_phases = enabled_metric_phases
+        self._save_best_model = save_best_model
 
     def initialize_parameters(self, client_manager: ClientManager) -> Parameters | None:
-        return self.strategy.initialize_parameters(client_manager)
+        return self._strategy.initialize_parameters(client_manager)
 
     def configure_fit(
         self, server_round: int, parameters: Parameters, client_manager: ClientManager
     ) -> list[tuple[ClientProxy, FitIns]]:
-        return self.strategy.configure_fit(server_round, parameters, client_manager)
+        return self._strategy.configure_fit(server_round, parameters, client_manager)
 
     def aggregate_fit(
         self,
@@ -54,25 +66,40 @@ class MetricStorageStrategy(Strategy):
         results: list[tuple[ClientProxy, FitRes]],
         failures: list[tuple[ClientProxy, FitRes] | BaseException],
     ) -> tuple[Parameters | None, dict[str, Scalar]]:
-        parameters, metrics = self.strategy.aggregate_fit(
+        """Aggregate fit results and log metrics.
+
+        If the `save_best_model` is enabled, then the aggregated parameters will
+            be kept in memory to be used later on if they represent the best model.
+        If logging is enabled with `AGGREGATE_FIT`, then it will log the metrics to
+            the given metric storage.
+
+        Args:
+            server_round: The current round of federated learning.
+            results: Successful fit results from clients.
+            failures: Failures from clients during fitting.
+
+        Returns:
+            A tuple containing the aggregated parameters and a dictionary of metrics.
+        """
+        parameters, metrics = self._strategy.aggregate_fit(
             server_round, results, failures
         )
-        if self.save_best_model:
+        if self._save_best_model:
             if parameters is None:
                 log(
                     level=WARNING,
                     msg="No model parameter provided, best model will not be saved.",
                 )
             else:
-                self.metrics_storage.update_current_round_model(parameters)
-        if MetricPhases.AGGREGATE_FIT in self.enabled_metric_phases:
-            self.metrics_storage.write_metrics(server_round, metrics)
+                self._metrics_storage.update_current_round_model(parameters)
+        if MetricPhases.AGGREGATE_FIT in self._enabled_metric_phases:
+            self._metrics_storage.write_metrics(server_round, metrics)
         return (parameters, metrics)
 
     def configure_evaluate(
         self, server_round: int, parameters: Parameters, client_manager: ClientManager
     ) -> list[tuple[ClientProxy, EvaluateIns]]:
-        return self.strategy.configure_evaluate(
+        return self._strategy.configure_evaluate(
             server_round, parameters, client_manager
         )
 
@@ -82,26 +109,63 @@ class MetricStorageStrategy(Strategy):
         results: list[tuple[ClientProxy, EvaluateRes]],
         failures: list[tuple[ClientProxy, EvaluateRes] | BaseException],
     ) -> tuple[float | None, dict[str, Scalar]]:
-        evaluation, metrics = self.strategy.aggregate_evaluate(
+        """Aggregate evaluation results and log metrics.
+
+        If the `save_best_model` is enabled, then the last best evaluation is compared
+            with the current evaluation to log the parameters of the best model.
+        If logging is enabled with `AGGREGATE_EVALUATE`, then it will log the metrics to
+            the given metric storage.
+
+        Args:
+            server_round: The current round of federated learning.
+            results: Successful evaluation results from clients.
+            failures: Failures from clients during evaluation.
+
+        Returns:
+            A tuple containing the aggregated loss and a dictionary of metrics.
+        """
+        evaluation, metrics = self._strategy.aggregate_evaluate(
             server_round, results, failures
         )
-        if MetricPhases.AGGREGATE_EVALUATE in self.enabled_metric_phases:
+        if self._save_best_model:
+            if evaluation is None:
+                log(
+                    level=WARNING,
+                    msg="No metric provided for evaluation, best model will not be saved.",
+                )
+            else:
+                self._metrics_storage.update_best_model(
+                    server_round=server_round, loss=evaluation
+                )
+        if MetricPhases.AGGREGATE_EVALUATE in self._enabled_metric_phases:
             if evaluation is not None:
-                self.metrics_storage.write_metrics(
+                self._metrics_storage.write_metrics(
                     server_round, {"loss_aggregated": evaluation}
                 )
-            self.metrics_storage.write_metrics(server_round, metrics)
+            self._metrics_storage.write_metrics(server_round, metrics)
         return (evaluation, metrics)
 
     def evaluate(
         self, server_round: int, parameters: Parameters
     ) -> tuple[float, dict[str, Scalar]] | None:
-        evaluation_result = self.strategy.evaluate(server_round, parameters)
-        if MetricPhases.EVALUATE in self.enabled_metric_phases:
+        """Evaluate model parameters on the server and log metrics.
+
+        If logging is enabled with `EVALUATE`, then it will log the metrics to
+            the given metric storage.
+
+        Args:
+            server_round: The current round of federated learning.
+            parameters: The current global model parameters to be evaluated.
+
+        Returns:
+            An optional tuple containing the loss and a dictionary of metrics from the evaluation.
+        """
+        evaluation_result = self._strategy.evaluate(server_round, parameters)
+        if MetricPhases.EVALUATE in self._enabled_metric_phases:
             if evaluation_result is None:
                 return None
-            self.metrics_storage.write_metrics(
+            self._metrics_storage.write_metrics(
                 server_round, {"loss": evaluation_result[0]}
             )
-            self.metrics_storage.write_metrics(server_round, evaluation_result[1])
+            self._metrics_storage.write_metrics(server_round, evaluation_result[1])
         return evaluation_result
