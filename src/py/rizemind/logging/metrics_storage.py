6b03b3e96a37495b345c67148321e53463f61fd8
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-18 10:31:03 -0400

    ensure MetricsStorage conforms to BaseMetricsStorage

--- src/py/rizemind/logging/metrics_storage.py
+++ src/py/rizemind/logging/metrics_storage.py
@@ -1,15 +1,16 @@
 import csv
-import json
 import os
 from datetime import datetime
-from logging import ERROR
 from pathlib import Path
 
-from flwr.common import Scalar, log
+import numpy as np
+import polars as pl
+from flwr.common import Parameters, Scalar, parameters_to_ndarrays
 from flwr.common.typing import UserConfigValue
+from rizemind.logging.base_metrics_storage import BaseMetricsStorage
 
 
-class MetricsStorage:
+class MetricsStorage(BaseMetricsStorage):
     def __init__(self, dir: Path, app_name: str) -> None:
         current_time = datetime.now().strftime("%Y-%m-%d-%H-%m-%S")
         self.dir = dir.joinpath(app_name, current_time)
@@ -17,29 +18,39 @@ class MetricsStorage:
 
         self.config_file = self.dir.joinpath("config.json")
         self.metrics_file = self.dir.joinpath("metrics.csv")
+        self.weights_file = self.dir.joinpath("weights.npz")
 
-        headers = ["round", "metric", "value"]
+        headers = ["server_round", "metric", "value"]
 
         with open(self.metrics_file, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerow(headers)
 
+        self._best_loss = np.inf
+        self._current_round_model = Parameters(tensors=[], tensor_type="")
+
     def write_config(self, config: dict[str, UserConfigValue]):
+        new_config_df = pl.from_dicts([config])
+
         file_exists_and_has_content = (
             os.path.exists(self.config_file) and os.path.getsize(self.config_file) > 0
         )
         if file_exists_and_has_content is True:
-            with open(self.config_file) as f:
-                existing_config: dict = json.load(f)
-                for key, value in existing_config.items():
-                    config[key] = value
-        try:
-            with open(self.config_file, mode="w") as f:
-                json.dump(config, f)
-        except OSError as e:
-            log(ERROR, f"Error writing config JSON to {self.config_file}: {e}")
+            config_df = pl.read_json(self.config_file)
+            merged_df = pl.concat([config_df, new_config_df], how="diagonal_relaxed")
+            new_config_df = merged_df
+        new_config_df.write_json(self.config_file)
 
-    def write_metrics(self, round: int, metrics: dict[str, Scalar]):
+    def write_metrics(self, server_round: int, metrics: dict[str, Scalar]):
         with open(self.metrics_file, "a", encoding="utf-8") as f:
             for metric, value in metrics.items():
-                csv.writer(f).writerow([round, metric, value])
+                csv.writer(f).writerow([server_round, metric, value])
+
+    def update_current_round_model(self, parameters: Parameters):
+        self._current_round_model = parameters
+
+    def update_best_model(self, server_round: int, loss: float):
+        if loss < self._best_loss:
+            self._best_loss = loss
+            ndarray_params = parameters_to_ndarrays(self._current_round_model)
+            np.savez(self.weights_file, ndarray_params)
