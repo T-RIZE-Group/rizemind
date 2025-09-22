72fb05f8981f83af78da8a9576f1719f4bb82af7
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-22 15:46:50 -0400

    Add init for signature authentication, along with docstrings

--- src/py/rizemind/authentication/signatures/eip712.py
+++ src/py/rizemind/authentication/signatures/eip712.py
@@ -1,11 +1,28 @@
 from dataclasses import dataclass
 from typing import Any, Protocol
 
-from eth_account.messages import encode_typed_data
+from eth_account.messages import SignableMessage, encode_typed_data
 from eth_typing import ChecksumAddress
 
 
+# @TODO check with mick for "domain separator"
 class EIP712DomainRequiredFields(Protocol):
+    """Protocol defining the required fields for an EIP-712 domain.
+
+    This protocol specifies the mandatory fields that must be present in any
+    EIP-712 domain implementation to ensure compatibility with the EIP-712
+    standard for typed structured data hashing and signing.
+
+    Attributes:
+        name: A human-readable name for the domain. This is typically
+        the name of the DApp or protocol.
+        version: The current version of the domain.
+        chainId: The EIP-155 chain ID of the network where the contract is
+        deployed.
+        verifyingContract: The Ethereum address of the contract that will
+        verify the signature.
+    """
+
     name: str
     version: str
     chainId: int
@@ -14,6 +31,22 @@ class EIP712DomainRequiredFields(Protocol):
 
 @dataclass
 class EIP712DomainStruct:
+    """Dataclass implementation of EIP-712 domain fields.
+
+    This class provides a concrete implementation of the EIP-712 domain
+    separator structure, containing all required fields for creating
+    typed structured data signatures according to the EIP-712 standard.
+
+    Attributes:
+        name: A human-readable name for the domain. This is typically
+        the name of the DApp or protocol.
+        version: The current version of the domain.
+        chainId: The EIP-155 chain ID of the network where the contract is
+        deployed.
+        verifyingContract: The Ethereum address of the contract that will
+        verify the signature.
+    """
+
     name: str
     version: str
     chainId: int
@@ -33,20 +66,16 @@ EIP712DomainABI = [
 def prepare_eip712_domain(
     chainid: int, version: str, contract: ChecksumAddress, name: str
 ) -> EIP712DomainRequiredFields:
-    """
-    Prepares the EIP-712 domain object for signing typed structured data.
+    """Prepares the EIP-712 domain object for signing typed structured data.
 
     Args:
-        chainid (int): The ID of the blockchain network (e.g., 1 for Ethereum mainnet, 3 for Ropsten).
-        contract (str): The address of the verifying contract in hexadecimal format (e.g., "0xCcCCc...").
-        name (str): The human-readable name of the domain (e.g., "MyApp").
+        chainid: The ID of the blockchain network (e.g., 1 for Ethereum mainnet, 3 for Ropsten).
+        version: The current version of the domain.
+        contract: The address of the verifying contract in hexadecimal format (e.g., "0xCcCCc...").
+        name: The human-readable name of the domain (e.g., "MyApp").
 
     Returns:
-        dict: A dictionary representing the EIP-712 domain object with the following keys:
-              - "name": The human-readable name of the domain.
-              - "version": The version of the domain, which is always set to "1".
-              - "chainId": The ID of the blockchain network.
-              - "verifyingContract": The address of the contract that verifies the signature.
+        An object representing the EIP-712 domain.
     """
 
     return EIP712DomainStruct(
@@ -58,6 +87,14 @@ def prepare_eip712_domain(
 
 
 def domain_to_dict(domain: EIP712DomainRequiredFields) -> dict[str, Any]:
+    """Changes the type of the domain to python dictionary
+
+    Args:
+        domain: The `EIP712DomainRequiredFields` representation of the EIP-712 domain.
+
+    Returns:
+        The dictionary representation of the EIP-712 domain.
+    """
     return {
         "name": domain.name,
         "version": domain.version,
@@ -71,24 +108,18 @@ def prepare_eip712_message(
     primaryType: str,
     message: dict,
     types: dict = {},
-):
-    """
-    Prepares the EIP-712 structured message for signing and encoding using the provided parameters.
+) -> SignableMessage:
+    """Prepares the EIP-712 structured message for signing and encoding using the provided parameters.
 
     Args:
-        chainid (int): The ID of the blockchain network (e.g., 1 for Ethereum mainnet, 3 for Ropsten).
-        contract (str): The address of the verifying contract in hexadecimal format (e.g., "0xCcCCc...").
-        name (str): The human-readable name of the domain (e.g., the app or contract name).
-        round (int): The current round number of the model.
-        hash (str): The model hash, provided as a hexadecimal string, representing a bytes32 hash.
+        eip712_domain: The EIP-712 domain object.
+        primaryType: The name of the primary message type.
+        message: The structured data to be signed.
+        types: A dictionary defining the custom data types used in the message,
+        where keys are type names and values are their ABI definitions.
 
     Returns:
-        dict: A dictionary representing the EIP-712 structured message, ready for signing.
-              The message includes:
-              - `domain`: The EIP-712 domain object.
-              - `types`: The type definitions for the domain and message fields.
-              - `primaryType`: The primary data type being signed, which is "Model".
-              - `message`: The actual message containing the round and the model hash.
+        An SignableMessage object ready for signing.
     """
     eip712_message = {
         "types": {
