a175f416732df2e39c6afc7fbf07db08484e97a4
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-25 11:12:55 -0400

    update AccountConfig path in forge_fixture

--- tests/integration/forge_fixtures.py
+++ tests/integration/forge_fixtures.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import pytest
 from pydantic import HttpUrl
-from rizemind.authentication.config import AccountConfig
+from rizemind.authentication import AccountConfig
 from rizemind.web3.config import Web3Config
 
 from .forge_helper import start_anvil
