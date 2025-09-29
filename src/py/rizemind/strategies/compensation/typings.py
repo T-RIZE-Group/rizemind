860a61b4fdaed1ed5d4fbcc87969bce69bacc52e
MikaeelMF <mikaeelmayeli@gmail.com>
2025-09-29 13:35:07 -0400

    add docstring for compensation strategies

--- src/py/rizemind/strategies/compensation/typings.py
+++ src/py/rizemind/strategies/compensation/typings.py
@@ -4,6 +4,24 @@ from eth_typing import ChecksumAddress
 
 
 class SupportsDistribute(Protocol):
-    def distribute(
-        self, trainer_scores: list[tuple[ChecksumAddress, float]]
-    ) -> str: ...
+    """Protocol for objects that can distribute compensation to federated learning trainers.
+
+    This protocol defines the interface for reward distribution mechanisms in a
+    federated learning system. Implementers are responsible for distributing
+    compensation to trainers based on their participation scores.
+
+    The protocol supports blockchain-based reward distribution where trainers
+    are identified by their Ethereum addresses.
+    """
+
+    def distribute(self, trainer_scores: list[tuple[ChecksumAddress, float]]) -> str:
+        """Distribute compensation to trainers based on their scores.
+
+        Args:
+            trainer_scores: List of tuples containing trainer addresses and their
+            corresponding compensation scores. Addresses must be valid Ethereum
+            checksum addresses.
+
+        @TODO: Include return value explanation
+        """
+        ...
